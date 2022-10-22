403ec4fabe67eafa728f6a09e04d4eebd7b1958a
rofe <github@rofe.com>
2022-10-22 15:43:24 +0200

    feat(slideshow): highlight heading text after br

--- blocks/slideshow/slideshow.js
+++ blocks/slideshow/slideshow.js
@@ -1,6 +1,6 @@
 export default async function decorate(block) {
   const slides = [...block.children];
-  slides.forEach((slide, index) => {
+  slides.forEach((slide) => {
     const overlay = document.createElement('div');
     overlay.classList.add('slideshow-overlay');
     [...slide.firstElementChild.children].forEach((child) => {
@@ -20,21 +20,15 @@ export default async function decorate(block) {
       slide.classList.add('slideshow-slide');
     });
     if (overlay.children) {
-      const br = overlay.querySelector('h1 >  h2 > br');
+      const br = overlay.querySelector('h1 > br, h2 > br');
       if (br) {
-        // animate heading text after <br>
+        // highlight heading text after <br>
         const animateTextNode = br.nextSibling;
         if (animateTextNode) {
           const span = document.createElement('span');
-          
-          
+          span.classList.add('slideshow-highlight');
           span.textContent = animateTextNode.textContent;
-          if (!index) {
-            span.classList.add('slideshow-reveal');
-          }
           animateTextNode.replaceWith(span);
-          
-          
         }
       }
       slide.append(overlay);
@@ -43,25 +37,14 @@ export default async function decorate(block) {
 
   // setup for multiple slides
   if (slides.length > 1) {
-    let index = 0;
     setInterval(() => {
-      // remove heading animations
-      block.querySelectorAll('span.slideshow-reveal').forEach((span) => {
-        span.classList.remove('slideshow-reveal');
-      });
       if (block.scrollWidth - block.scrollLeft > block.offsetWidth) {
         // next slide
-        index += 1;
         block.scrollLeft += block.offsetWidth;
       } else {
         // back to start
-        index = 0;
         block.scrollLeft = 0;
       }
-      // add next heading animation
-      block.children[index].querySelectorAll('span').forEach((span) => {
-        span.classList.add('slideshow-reveal');
-      });
     }, 7000);
     window.addEventListener('resize', () => {
       block.scrollLeft = 0;
